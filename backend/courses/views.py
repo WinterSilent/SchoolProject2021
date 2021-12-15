89cb29218d25cb68864700f58ba7ec78ec169dd4
GlebZaytsev <winter1silent@gmail.com>
2021-12-15 11:31:37 +0500

    Исправлен вывод в json

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -8,25 +8,29 @@ from .models import Teacher, Course, Feedback
 def teacher_list(request):
     """Список учителей"""
     teachers = Teacher.objects.order_by('pk')[:5]
-    return JsonResponse({'result': [t.to_dict() for t in teachers]})
+    return JsonResponse({'result': [t.to_dict() for t in teachers]},
+                        safe=False, json_dumps_params={'ensure_ascii': False})
 
 
 @require_GET
 def course_list(request):
     """Список курсов"""
     courses = Course.objects.order_by('pk')
-    return JsonResponse({'result': [c.list_dict() for c in courses]})
+    return JsonResponse({'result': [c.list_dict() for c in courses]},
+                        safe=False, json_dumps_params={'ensure_ascii': False})
 
 
 @require_GET
 def course_read(request, course_id):
     """Информация по курсу"""
     course = get_object_or_404(Course, pk=course_id)
-    return JsonResponse({'result': course.read_dict()})
+    return JsonResponse({'result': course.read_dict()},
+                        safe=False, json_dumps_params={'ensure_ascii': False})
 
 
 @require_GET
 def feedback_list(request):
     """Список курсов"""
     query = Feedback.objects.order_by('pk')[:5]
-    return JsonResponse({'result': [rec.to_dict() for rec in query]})
+    return JsonResponse({'result': [rec.to_dict() for rec in query]},
+                        safe=False, json_dumps_params={'ensure_ascii': False})
