49f674b3ce2d5afa9e0a4c7a89f55925b4dd898d
valeriailikbaeva <valeria180501@mail.ru>
2021-12-16 08:32:01 +0500

    header and faq small corrections

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,7 +4,7 @@ class Header {
         <div class="header__container">
           <div class="header__content">
             <div class="header__logo">
-              <a class="header__a" href="index.html">ТЕНЗОР</a>
+              <a class="header__a" href="index.html">ТЕНЗОР <br><span class="header__colortext">EDUCATION</span></a>
             </div>
             <div class="header__menu">
               <a class="header__a" href="#about-anchor">О нас</a>
